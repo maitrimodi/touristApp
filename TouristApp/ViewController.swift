7a7bca52bc482087fd9a10d2ea303315da235b1e
maitrimodi <maitrimodi19@gmail.com>
2021-04-02 17:06:01 +0530

    Initial commit

---
+++ TouristApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TouristApp
+//
+//  Created by maitri Modi on 2021-04-02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
